b50d973a827b89a49dd4ce8cf53f97e511b8d936
Volodia68 <116280280+Volodia68@users.noreply.github.com>
2024-03-13 15:26:26 +0300

    poizon task compilte

---
+++ Diploma/Assets/CheckColor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckColor : MonoBehaviour
+{
+    public Image color;
+    public Button Bottle;
+    public PoizonTask NamingIvante;
+    private void Awake()
+    {
+        Bottle.onClick.AddListener(Cheking);
+        Bottle.onClick.AddListener(() => NamingIvante.Cheking(color.color));
+
+    }
+
+    private void Cheking()
+    {
+        if (color.color == Color.red)
+        {
+            Bottle.interactable = false;
+        } 
+    }
+}
--- Diploma/Assets/PoizonTask.cs
+++ Diploma/Assets/PoizonTask.cs
@@ -1,22 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PoizonTask : MonoBehaviour
 {
-    [SerializeField] private GameObject[] Bottle; 
+    [SerializeField] GameObject menu;
+    [SerializeField] GameObject exit;
+    [SerializeField] float pointsByTask;
     public float timeRemaining = 10;
     private bool timerIsRunning = false;
     [SerializeField] private TextMeshProUGUI timeText;
+    public List<CheckColor> bottle;
+    private System.Random random = new System.Random();
+    int t = 2;
 
     public void StartTimer()
     {
-        // Starts the timer automatically
+        
         timerIsRunning = true;
     }
+    
+    public void Cheking(UnityEngine.Color color)
+    {
+        
+        if(t != 0 && color == UnityEngine.Color.red)
+        {
+            t--; 
+        }
+        else if(t == 0 )
+        {
+            menu.GetComponent<TaskChanger>().AddPointsToSlider(pointsByTask);
+            menu.SetActive(true);
+            exit.SetActive(false);
+        }
 
+    }
     void Update()
     {
         if (timerIsRunning)
@@ -26,9 +48,19 @@ public class PoizonTask : MonoBehaviour
                 timeRemaining -= Time.deltaTime;
                 DisplayTime(timeRemaining);
             }
+            
             else
             {
-                
+                int count = 0;
+                while (count < 3)
+                {
+                    int r = random.Next(0, 5);
+                    if (bottle[r].color.color != UnityEngine.Color.red)
+                    {
+                        count++;
+                        bottle[r].color.color = UnityEngine.Color.red;
+                    }
+                }
                 timeRemaining = 0;
                 timerIsRunning = false;
             }
